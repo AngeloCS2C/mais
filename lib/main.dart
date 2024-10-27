6f8a334f685a7fd5e2266fe06cf14bd37fb2d612
0xkenn <kennycharlest@gmail.com>
2024-10-28 02:01:11 +0800

    wowbi

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Ma Iscanner',
       theme: ThemeData(primarySwatch: Colors.green),
       home: SplashScreen(camera: camera), // Pass camera to SplashScreen
